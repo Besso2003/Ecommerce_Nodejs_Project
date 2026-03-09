a2e07980f6d89921888fda55d320443fa3134349
Rehap1 <rehabhamed806@gmail.com>
2026-03-09 13:56:59 +0300

    fix bug

--- MiddleWare/validateToken.js
+++ MiddleWare/validateToken.js
@@ -1,16 +1,11 @@
 import jwt from "jsonwebtoken";
 
 let validateToken = (req, res, next) => {
-    // let token = req.headers.token;
-    // instead of token because bearer token reads from  header.auth
-    let authHeader = req.headers.authorization;
-    if (!authHeader) {
+    let token = req.headers.token;
+    if (!token) {
         return res.status(401).json({ message: "No token provided" });
     }
-
-    // Extract the actual token
-    let token = authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : authHeader;
-
+    
     jwt.verify(token, "secret", async (err, decoded_token) => {
         if (err) {
             return res.status(401).json({ message: "Invalid Token" })
